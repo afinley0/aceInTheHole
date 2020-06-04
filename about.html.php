9c1bcf9300a379c1e12065b956358fa198732822
afinley0 <63321014+afinley0@users.noreply.github.com>
2020-06-04 16:55:28 -0700

    Add files via upload

---
+++ about.html.php
@@ -0,0 +1,82 @@
+<!DOCTYPE html>
+<html lang="en">
+
+<head>
+    <meta charset="utf-8">
+    <meta name="author" content="Amy Finley">
+
+    <!-- Title -->
+    <title>About : Ace in the Hole Multisport Events</title>
+
+    <!-- Stylesheet -->
+    <link href="../css/reset.css" rel="stylesheet" type="text/css">
+    <link href="../css/helper.css" rel="stylesheet" type="text/css">
+    <link href="../css/grid.css" rel="stylesheet" type="text/css">
+    <link href="../css/style.css" rel="stylesheet" type="text/css">
+    <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/4.7.0/css/font-awesome.min.css">
+    <meta name="viewport" content="width=device-width, initial-scale=1">
+
+    <!-- Google Fonts -->
+    <link href="https://fonts.googleapis.com/css2?family=Roboto+Slab&display=swap" rel="stylesheet">
+    <link href="https://fonts.googleapis.com/css2?family=Montserrat:wght@500&display=swap" rel="stylesheet">
+
+    <!-- Font Awesome -->
+    <script src="https://kit.fontawesome.com/072aa1ce93.js" crossorigin="anonymous"></script>
+</head>
+
+<body>
+    <?php include '../includes/header.inc.html.php'; ?>
+    <?php include '../includes/nav.inc.html.php'; ?>
+
+    <main>
+
+        <?php include '../includes/sidebar.inc.html.php'; ?>
+        <h1>About</h1>
+        <div class="content_two">
+            <!-- Image -->
+            <img class="homeImg" src="../images/img2.jpg" alt="Image of a race participant.">
+
+            <!-- Text -->
+            <br>
+            <h2>The Events</h2>
+            <p>There is something for every level of athletic ability. The weekend includes a
+                first timer triathlon, a sprint, Olympic, and Half-Iron triathlons and 10K and
+                Half marathon runs. Come to experience your first race or come to compete to win,
+                but make sure you come to have fun!</p>
+            <br>
+
+
+            <h2>What to Bring</h2>
+            <p><em>Watch the weather closely. The show goes on no matter what the weather is doing.</em></p>
+            <p><em>Remember to bring a change of clothing so you can enjoy the post-event festivities.</em></p>
+            <p>You must wear your chip timing piece during the entire event. You will be given a
+                band that will hold your chip timing piece around your ankle throughout the entire
+                event. Be sure that it is snapped tightly. Be sure to have your Chip Timing piece on
+                before you start the race and be sure to step over the timing mats after each segment
+                of the race. If you are wearing a wet-suit, make sure the timing piece goes under
+                your wetsuit otherwise, you will have a very difficult time getting off your wet suit.</p>
+            <h3>Swim:</h3>
+            <p>Wetsuits are optional for the swim but will provide buoyancy and warmth. However,
+                many people opt for no wetsuit for a triathlon so no worries. We will provide
+                you with a swim cap but you will want to bring your own goggles.</p>
+            <h3>Bike:</h3>
+            <p>A biking helmet is mandatory. You will also receive 2 stickers in your package with
+                your race number on them. The small sticker should go on the front of your helmet.
+                The bigger sticker will wrap around your bike frame. Road or mountain bikes are
+                acceptable. For safety reasons, be sure to have end-caps on the end of your
+                handle-bars.</p>
+            <h3>Run:</h3>
+            <p>You must finish the race with your bib number on the front of you. Some people choose
+                to pin it on at the beginning of the race and have it on for the whole event so they
+                don't have to worry about it. Others pin it on a singlet that they put on once they
+                finish the swim before they head out for the bike. Others use an elastic racing strap
+                that they pin their bib number to and then quickly strap it on before they leave for
+                the run. Use whatever option feels best for you.</p>
+            <br>
+        </div>
+        <div class="clear"></div>
+    </main>
+    <?php include '../includes/footer.inc.html.php'; ?>
+</body>
+
+</html>
