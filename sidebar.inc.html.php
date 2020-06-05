8da3cf815c7bbea7a51435541a03f3529bd7745f
afinley0 <63321014+afinley0@users.noreply.github.com>
2020-06-04 17:03:28 -0700

    Add files via upload

---
+++ sidebar.inc.html.php
@@ -0,0 +1,35 @@
+<!-- Sidebar -->
+
+<div class="sidebar">
+    <div class="center">
+        <div id="fb-root"></div>
+        <script async defer crossorigin="anonymous" src="https://connect.facebook.net/en_US/sdk.js#xfbml=1&version=v7.0"></script>
+        <h3>Social Media Feed</h3>
+        <div class="fb-page" data-href="https://www.facebook.com/Cas222Aceinthehole-110661963841617" data-tabs="timeline" data-width="" data-height="" data-small-header="true" data-adapt-container-width="true" data-hide-cover="true" data-show-facepile="true">
+            <blockquote cite="https://www.facebook.com/Cas222Aceinthehole-110661963841617" class="fb-xfbml-parse-ignore">
+                <a href="https://www.facebook.com/Cas222Aceinthehole-110661963841617">Cas222Aceinthehole</a>
+            </blockquote>
+        </div>
+    </div>
+    <br>
+    <div class="center">
+        <h3>Weather Feed</h3>
+        <a class="weatherwidget-io" href="https://forecast7.com/en/45d52n122d68/portland/?unit=us" data-label_1="PORTLAND" data-label_2="WEATHER" data-font="Roboto Slab" data-icons="Climacons" data-days="3" data-theme="original" data-basecolor="#010203">PORTLAND WEATHER</a>
+        <script>
+            ! function(d, s, id) {
+                var js, fjs = d.getElementsByTagName(s)[0];
+                if (!d.getElementById(id)) {
+                    js = d.createElement(s);
+                    js.id = id;
+                    js.src = 'https://weatherwidget.io/js/widget.min.js';
+                    fjs.parentNode.insertBefore(js, fjs);
+                }
+            }(document, 'script', 'weatherwidget-io-js');
+        </script>
+    </div>
+
+    <div class="center">
+        <a href="https://www.facebook.com/Cas222Aceinthehole-110661963841617/"><i class="fab fa-facebook-square"></i></a>
+        <a href="https://twitter.com/pcccas222?lang=en"><i class="fab fa-twitter"></i></a>
+    </div>
+</div>
