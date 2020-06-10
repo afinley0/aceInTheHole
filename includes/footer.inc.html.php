7b53adf9718cd7a14bce7d0cc4f690d60468367a
afinley0 <63321014+afinley0@users.noreply.github.com>
2020-06-09 18:11:38 -0700

    Add files via upload

---
+++ includes/footer.inc.html.php
@@ -0,0 +1,27 @@
+<footer>
+
+    <!-- Footer navigation -->
+    <!-- Footer nav goes to these places : Contact | Register | FAQ | About | Packet Info | Events -->
+    <!-- Links for FAQ & Packet info will go directly to that part of the page -->
+    <div class="center">
+        <a href="https://www.facebook.com/Cas222Aceinthehole-110661963841617/"><i class="fab fa-facebook-square"></i></a>
+        <a href="https://twitter.com/pcccas222?lang=en"><i class="fab fa-twitter"></i></a>
+    </div>
+    <div class="footercol footer-nav">
+        <ul class="fnav">
+            <li class="fnav"><a class="fnav" href="<?php echo ACE_URL; ?>about/about.html.php">About</a></li>
+            <li class="fnav"><a class="fnav" href="<?php echo ACE_URL; ?>register">Register</a></li>
+            <li class="fnav"><a class="fnav" href="<?php echo ACE_URL; ?>contact">Contact Us</a></li>
+        </ul>
+        <ul class="fnav">
+            <li class="fnav"><a class="fnav" href="<?php echo ACE_URL; ?>events">Events</a></li>
+            <li class="fnav"><a class="fnav" href="<?php echo ACE_URL; ?>events/events.html.php#Packet">Packet Info</a></li>
+            <li class="fnav"><a class="fnav" href="<?php echo ACE_URL; ?>faq">FAQ</a></li>
+        </ul>
+    </div>
+
+    <!-- Social Media Icons & Copyright Info -->
+    <i class="far fa-copyright fa-lg"></i> 2020
+
+</footer>
+
---
+++ includes/nav.inc.html.php
@@ -0,0 +1,29 @@
+<!-- Sticky Navigation -->
+<nav class="navbar">
+    <a href="<?php echo ACE_URL; ?>">Home</a>
+    <a href="<?php echo ACE_URL; ?>about">About</a>
+    <a href="<?php echo ACE_URL; ?>events">Events</a>
+    <a href="<?php echo ACE_URL; ?>faq">FAQ</a>
+    <a href="<?php echo ACE_URL; ?>register">Register</a>
+    <a href="<?php echo ACE_URL; ?>contact">Contact</a>
+
+</nav> <!-- End Navigation -->
+
+<!-- Hamburger Menu -->
+<div class="topnav">
+    <a href="<?php echo ACE_URL; ?>" class="active">Ace in the Hole MultiSport Events</a>
+    <div id="myLinks">
+        <a href="<?php echo ACE_URL; ?>">Home</a>
+        <a href="<?php echo ACE_URL; ?>about">About</a>
+        <a href="<?php echo ACE_URL; ?>events">Events</a>
+        <a href="<?php echo ACE_URL; ?>faq">FAQ</a>
+        <a href="<?php echo ACE_URL; ?>register">Register</a>
+        <a href="<?php echo ACE_URL; ?>contact">Contact</a>
+    </div>
+    <!-- "Hamburger menu" / "Bar icon" to toggle the navigation links -->
+    <a href="javascript:void(0);" class="icon" onclick="myFunction()">
+        <i class="fa fa-bars"></i>
+    </a>
+</div> <!-- End Hamburger Menu -->
+
+</header>
