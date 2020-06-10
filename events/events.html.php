6f9c1fc89d05fbd0263932e8045f6c170bdc1de5
afinley0 <63321014+afinley0@users.noreply.github.com>
2020-06-09 18:08:03 -0700

    Add files via upload

---
+++ events/events.html.php
@@ -0,0 +1,175 @@
+<!DOCTYPE html>
+<html lang="en">
+
+<head>
+    <meta charset="utf-8">
+    <meta name="author" content="Amy Finley">
+
+    <!-- Title -->
+    <title>Events : Ace in the Hole Multisport Events</title>
+
+    <!-- Stylesheet -->
+    <link href="../css/reset.css" rel="stylesheet" type="text/css">
+    <link href="../css/helper.css" rel="stylesheet" type="text/css">
+    <link href="../css/grid.css" rel="stylesheet" type="text/css">
+    <link href="../css/style.css" rel="stylesheet" type="text/css">
+    <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/4.7.0/css/font-awesome.min.css">
+    <meta name="viewport" content="width=device-width, initial-scale=1">
+
+    <!-- Google Fonts -->
+    <link href="https://fonts.googleapis.com/css2?family=Roboto+Slab&display=swap" rel="stylesheet">
+    <link href="https://fonts.googleapis.com/css2?family=Montserrat:wght@500&display=swap" rel="stylesheet">
+
+    <!-- Font Awesome -->
+    <script src="https://kit.fontawesome.com/072aa1ce93.js" crossorigin="anonymous"></script>
+</head>
+
+<body>
+    <?php include '../includes/header.inc.html.php'; ?>
+    <?php include '../includes/nav.inc.html.php'; ?>
+
+    <main>
+        <?php include '../includes/sidebar.inc.html.php'; ?>
+        <h1>Events</h1>
+        <div class="content_two">
+
+            <!-- Place holder image -->
+            <img class="homeImg" src="../images/img3.jpg" alt="Image of a race participant.">
+
+            <!-- Text -->
+            <br>
+            <p>Ace in the Hole Multisport Events is proud to offer a two day event full of running and triathlons.
+                Saturday and Sunday will feature different events, so be sure to register for the events you want.</p>
+            <p>All events are held in the morning. </p>
+            <div class="center">
+                <div class="event">
+                    <h3>Saturday Events</h3>
+                    <table>
+                        <tr>
+                            <th>Event</th>
+                            <th>Time</th>
+                            <th>Price</th>
+                        </tr>
+                        <tr>
+                            <td>Long Course Triathlon</td>
+                            <td>7:00</td>
+                            <td>$240</td>
+                        </tr>
+                        <tr>
+                            <td>Olympic Triathlon</td>
+                            <td>7:30</td>
+                            <td>$110</td>
+                        </tr>
+                        <tr>
+                            <td>10K</td>
+                            <td>7:15</td>
+                            <td>$50</td>
+                        </tr>
+                        <tr>
+                            <td>Half Marathon</td>
+                            <td>7:15</td>
+                            <td>$75</td>
+                        </tr>
+                    </table>
+                    <br>
+                    <h3>Saturday Events</h3>
+                    <table>
+                        <tr>
+                            <th>Event</th>
+                            <th>Time</th>
+                            <th>Price</th>
+                        </tr>
+                        <tr>
+                            <td>Sprint Triathlon</td>
+                            <td>8:00</td>
+                            <td>$90</td>
+                        </tr>
+                        <tr>
+                            <td>Try-a-Tri</td>
+                            <td>8:20</td>
+                            <td>$65</td>
+                        </tr>
+                        <tr>
+                            <td>10K</td>
+                            <td>7:15</td>
+                            <td>$50</td>
+                        </tr>
+                        <tr>
+                            <td>Splash n Dash</td>
+                            <td>Noon</td>
+                            <td>$25</td>
+                        </tr>
+                    </table>
+                </div>
+            </div>
+            <h3>Cost Includes</h3>
+            <div class="faq">
+                <ul>
+                    <li class="faq">Food & Beer </li>
+                    <li class="faq">Access to the weekend's live entertainment & Fitness Expo</li>
+                    <li class="faq">Commemorative Finisher medal</li>
+                    <li class="faq">Accurate Chip Timing for competitive races</li>
+                    <li class="faq">Ace in the Hole Multisport Weekend Tech Shirt</li>
+                    <li class="faq">Post-event party & entertainment</li>
+                </ul>
+                <strong>*NOTE: Tech shirts guaranteed to pre-registered participants only.</strong>
+            </div>
+            <h2>Course Details</h2>
+            <p><strong class="cd">WATER TEMPERATURE</strong> is expected to be between 62 & 66 degrees. The temperature
+                will be taken on Friday and the morning of the race. Wetsuits are recommended.</p>
+            <p><strong class="cd">Long Course SWIM - 1.2mi:</strong> Participants will make two counter-clockwise loops.
+                Large buoys will mark the turn points. Kayakers will be positioned on the water to support the
+                swimmers. Medical support will be present on the beach</p>
+            <p><strong class="cd">Long Course BIKE - 58 Miles:</strong> A scenic point-to-point course that travels over
+                gently rolling hills prior to three hard climbs. The bike course will be marked with large
+                directional signage and there will be course marshals at key intersections to help direct
+                you.</p>
+            <p><strong class="cd">Long Course RUN - 13.1mi:</strong> A mostly flat loop course on widely paved bike
+                paths that traverse through and around this beautiful and scenic destination resort (two hills
+                total with a minimal elevation gain)</p>
+            <p><strong class="cd">OLYMPIC SWIM - 1,500 meters:</strong> Participants will make two counter-clockwise
+                loops. Large buoys will mark the turn points. Kayakers will be positioned on the water to
+                support the swimmers. Medical support will be present on the beach.</p>
+            <p><strong class="cd">OLYMPIC BIKE - 28mi:</strong> A scenic point-to-point course that travels over gently
+                rolling hills. The bike course will be marked with large directional signage and course
+                marshals will be present at key intersections.</p>
+            <p><strong class="cd">OLYMPIC RUN - 10K:</strong> A mostly flat loop course on widely paved bike paths that
+                traverse through and around this beautiful and scenic destination resort (one hill total with
+                a minimal elevation gain).</p>
+            <p><strong class="cd">Sprint:</strong> Course will offer a 1-loop 1/2 mile swim, the exact same 28 mile Bike
+                Course as the Olympic distance and a 5km run.</p>
+            <p><strong class="cd">Try-A-Tri:</strong>This novice race is designed for the first time triathlete, those
+                new to the sport, our Junior Triathletes. The swim is a shorter, more manageable 1/4 mile
+                distance, (instead of the standard 1/2-mile Sprint distance swim), 10 mile bike ride (vs
+                12 miles and it's a 2 loop course making it very spectator friendly!) and a flat 2 mile run
+                (vs 3 mile sprint course).</p>
+            <p><strong class="cd">Half Marathon COURSE:</strong> Half-Marathon (13.1-miles): Half-Marathon event starts
+                and finishes in the Athletes Village to the cheers of the enthusiastic crowd. Once finished,
+                runners can enjoy the finish line festivities, including the Sports & Fitness Expo and live
+                entertainment. Post-race refreshments will be provided and the Awards Ceremony for the
+                Half-Marathon will begin once the results have been certified</p>
+            <p><strong class="cd">10k COURSE:</strong> The 10K event starts and finishes in the Athletes Village. The
+                paths are approximately 6 ft wide, perfectly paved and wind around through the forest. Each
+                course has only two small hills with a minimal elevation gain to navigate and a fast downhill
+                to flat finish to the roaring cheers of the crowd.</p>
+            <p><strong class="cd">Splash n' Dash:</strong> Kids event for participants ages 4 to 15. This is a 100 yard
+                swim and a 50 yard dash. It is free if an adult registers for an event otherwise it is $25.</p>
+
+            <a name="Packet"></a>
+            <h2>Packet Pick Up</h2>
+            <p>All packet pick up will occur at: <br>
+                <div class="address_box">
+                    <a class="address" href="https://goo.gl/maps/EzEAyi1yNtrdcTpg9">Why Worry Racing <br>
+                        123 NW Everett <br>
+                        Portland OR</a>
+                </div>
+                <br>
+                <p>Packet pick up hours: Thursday 8-5, Friday 9-noon. No day of event packet pick up.
+                </p>
+        </div>
+        <div class="clear"></div>
+    </main>
+    <?php include '../includes/footer.inc.html.php'; ?>
+</body>
+
+</html>
