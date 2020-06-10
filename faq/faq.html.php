0ac2d6a4d69073d3fd9419a855e5ca6e16614b42
afinley0 <63321014+afinley0@users.noreply.github.com>
2020-06-09 18:08:38 -0700

    Add files via upload

---
+++ faq/faq.html.php
@@ -0,0 +1,78 @@
+<!DOCTYPE html>
+<html lang="en">
+
+<head>
+    <meta charset="utf-8">
+    <meta name="author" content="Amy Finley">
+
+    <!-- Title -->
+    <title>FAQ : Ace in the Hole Multisport Events</title>
+
+    <!-- Stylesheet -->
+    <link href="../css/reset.css" rel="stylesheet" type="text/css">
+    <link href="../css/helper.css" rel="stylesheet" type="text/css">
+    <link href="../css/grid.css" rel="stylesheet" type="text/css">
+    <link href="../css/style.css" rel="stylesheet" type="text/css">
+    <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/4.7.0/css/font-awesome.min.css">
+    <meta name="viewport" content="width=device-width, initial-scale=1">
+
+    <!-- Google Fonts -->
+    <link href="https://fonts.googleapis.com/css2?family=Roboto+Slab&display=swap" rel="stylesheet">
+    <link href="https://fonts.googleapis.com/css2?family=Montserrat:wght@500&display=swap" rel="stylesheet">
+
+    <!-- Font Awesome -->
+    <script src="https://kit.fontawesome.com/072aa1ce93.js" crossorigin="anonymous"></script>
+</head>
+
+<body>
+    <?php include '../includes/header.inc.html.php'; ?>
+    <?php include '../includes/nav.inc.html.php'; ?>
+
+    <main>
+        <div class="content">
+            <?php include '../includes/sidebar.inc.html.php'; ?>
+            <h1>FAQ</h1>
+            <div class="content_two">
+                <!-- Place holder image -->
+                <img class="homeImg" src="../images/img4.jpg" alt="Image of a race participant.">
+                <h4>What are the Rules?</h4>
+                <p>We currently adhere to the USAT Rules for Triathlon & Duathlon. Important rules include
+                    no drafting, you must wear a helmet and music is ABSOLUTELY NOT allowed during the bike
+                    for obvious safety reasons.</p>
+                <h4>Can I use a personal music device while cycling?</h4>
+                <p>Absolutely no music devices are allowed during the bike segment for obvious safety risks
+                    and will result in immediate disqualification.</p>
+                <h4>Can I use a personal music device while running? </h4>
+                <p>During any running segments, we prefer for athletes to NOT use music devices to maximize
+                    safety, assure a fair competitive environment and assure athletes can hear all course
+                    marshal instructions. With that said, we also understand that many athletes rely on music
+                    to help endure the challenge of running. To accommodate the needs of our athletes while
+                    still assuring maximum safety and a fair, competitive environment, here is the arrangement
+                    that we can accommodate.</p>
+                <div class="faq">
+                    <ul class="faq">
+                        <li class="faq">If you chose to use a personal music device, you must always keep one ear open for
+                            instructions so only one ear-bud can be used at any time. In addition, if you opt to
+                            use music, you will not be eligible for awards, points and rankings. So as an athlete,
+                            you have to make a choice - music or awards/points/rankings.</li>
+                        <li class="faq">If you are using a personal music devise with two earbuds, you will be immediately
+                            disqualified. Trust us, we don't want to have to enforce a disqualification but in
+                            order to assure the safety of all athletes, this is a very important rule.</li>
+                        <li class="faq">We also request that if you chose to use a personal music device that you keep the
+                            volume to a minimum to assure all instructions can be heard.</li>
+                    </ul>
+                </div>
+                <h4>Do I need to wear a wetsuit?</h4>
+                <p>No, you do not need to wear a wetsuit. Many will not wear a wetsuit while others will
+                    choose to wear a wetsuit because of the buoyancy and warmth factor.</p>
+                <h4>Do I have to use a road or racing bike?</h4>
+                <p>No. We welcome any type of bike as long as it is functioning properly with brakes and
+                    endcaps at the end of your handlebars.</p>
+            </div>
+        </div>
+        <div class="clear"></div>
+    </main>
+    <?php include '../includes/footer.inc.html.php'; ?>
+</body>
+
+</html>
